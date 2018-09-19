042bc391cc7af9a16d8fe9e2db977f059b1477fb
Yuanxu Wu <sebastianwu@10-16-39-218.dynapool.wireless.nyu.edu>
2018-09-19 17:45:30 -0400

    initial UpdateCustomer class

---
+++ src/main/java/com/viome/interview/business/UpdateCustomers.java
@@ -0,0 +1,11 @@
+package com.viome.interview.business;
+
+// changes customers located in Vancouver to New York. 
+class UpdateCustomers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
